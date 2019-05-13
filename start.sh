c24ce487899c3978083c7179bcb2544b98c2185a
John Li <johnmarkli@gmail.com>
2019-05-13 18:04:08 +0000

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,14 @@
 # set up new ssh key with github
-ssh-keygen -t rsa -b 4096 -C "johnmarkli@gmail.com"
+SSH_KEY_NAME=id_rsa
+SSH_KEY_PATH=$HOME/.ssh/$SSH_KEY_NAME
+ssh-keygen -t rsa -b 4096 -C "johnmarkli@gmail.com" -f $SSH_KEY_PATH
 curl -u "johnmarkli" \
-    --data "{\"title\":\"j-dev`date +%Y%m%d%H%M%S`\",\"key\":\"`cat ~/.ssh/id_rsa.pub`\"}" \
-    https://api.github.com/user/keys
+  --data "{\"title\":\"j-dev`date +%Y%m%d%H%M%S`\",\"key\":\"`cat $SSH_KEY_PATH.pub`\"}" \
+  https://api.github.com/user/keys
+ssh-add $SSH_KEY_PATH
+sleep 3
 
 # Bootstrap dotfiles
 cd ~
 git clone git@github.com:johnmarkli/dotfiles.git
-BACKUP_ALL=true ~/dotfiles/bootstrap.sh
+[ -d dotfiles ] && BACKUP_ALL=true ~/dotfiles/bootstrap.sh
